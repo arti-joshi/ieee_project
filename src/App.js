690e79565b0776c55f3509d75e894b8cb0917dcb
coder21-space <kaushal2132002@gmail.com>
2024-09-21 17:41:14 +0530

    basic changes+heading part

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@
 import React from 'react';
 import './App.css'; 
 
+
+/*  function Heading() {
+  return (
+    <div className="App">
+      <h1 className="big-heading">This is a Big Heading</h1>      for heading part
+    </div>
+  );
+}
+  */
+
 const Card = ({ title, content, imageUrl }) => {
   return (
+
     <div className="card">
       {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
       <div className="card-body">
@@ -18,19 +29,49 @@ const App = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
       <Card
-        title="first card"
+        title="First Month Pregnant"
         content="IEEE project.."
-        imageUrl="https://via.placeholder.com/300"
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
       />
       <Card
-        title="second card"
+        title="Second Month Pregnant"
         content="IEEE project."
-        imageUrl="https://via.placeholder.com/300"
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
       />
       <Card
-        title="Third Card"
+        title="Third Month Pregnant"
         content="IEEE project."
-        imageUrl="https://via.placeholder.com/300"
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+      <Card
+        title="Forth Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+      <Card
+        title="Fifth Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+      <Card
+        title="Sixth  Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+       <Card
+        title="Seventh  Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+       <Card
+        title="Eight  Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
+      />
+       <Card
+        title="Ninth  Month Pregnant"
+        content="IEEE project."
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6b/Icecat1-300x300.svg"
       />
     </div>
   );
